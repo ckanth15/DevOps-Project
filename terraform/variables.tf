f0008d46f515b6b2d8d68d76d769bec58443b22f
ckanth15 <ckanthus@gmail.com>
2025-05-19 13:29:06 -0700

    reverted changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,10 +22,10 @@ variable "key_name" {
 
 }
 
-# Assuming your keys are stored in the following paths
-locals {
-    #public_key  = file("~/.ssh/id_rsa.pub")  # Path to your public key
-    public_key  = file("/Users/chandrawip-off/keyforawsdev.pub")  # Path to your public key
-    # private_key = file("~/.ssh/id_rsa")      # Path to your private key
-    private_key = file("/Users/chandrawip-off/keyforawsdev")      # Path to your private key
-}
+# # Assuming your keys are stored in the following paths
+# locals {
+#     #public_key  = file("~/.ssh/id_rsa.pub")  # Path to your public key
+#     public_key  = file("/Users/chandrawip-off/keyforawsdev.pub")  # Path to your public key
+#     # private_key = file("~/.ssh/id_rsa")      # Path to your private key
+#     private_key = file("/Users/chandrawip-off/keyforawsdev")      # Path to your private key
+# }
