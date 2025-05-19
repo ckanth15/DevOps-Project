ccf83c62d845a3305343b78d5a7fb2b6e997104c
ckanth15 <ckanthus@gmail.com>
2025-05-19 13:13:21 -0700

    Updated key locals

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,16 +4,25 @@ variable "region" {
 }
 
 # Add your key 
-variable "public_key" {
+# variable "public_key" {
 
-}
-
-
-variable "private_key" {
-
-}
+# }
 
 
-variable "key_name" {
+# variable "private_key" {
 
+# }
+
+
+# variable "key_name" {
+
+
+# }
+
+# Assuming your keys are stored in the following paths
+locals {
+    #public_key  = file("~/.ssh/id_rsa.pub")  # Path to your public key
+    public_key  = file("/Users/chandrawip-off/keyforawsdev.pub")  # Path to your public key
+    # private_key = file("~/.ssh/id_rsa")      # Path to your private key
+    private_key = file("/Users/chandrawip-off/keyforawsdev")      # Path to your private key
 }
